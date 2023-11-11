b8be980f4cd60a81170b1f3a942d43f12876a5a1
Oleg Zapara <ole9zp@gmail.com>
2023-11-11 20:14:51 +0200

    schedule table context menu

--- src/pages/SchedulePage/SchedulePage.jsx
+++ src/pages/SchedulePage/SchedulePage.jsx
@@ -1,6 +1,6 @@
 import { Column } from 'primereact/column'
 import './SchedulePage.css'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Accordion, AccordionTab } from 'primereact/accordion';
 import { DataTable } from 'primereact/datatable';
 import { InputText } from 'primereact/inputtext';
@@ -10,38 +10,30 @@ import { Dropdown } from 'primereact/dropdown';
 import { Tag } from 'primereact/tag';
 import { FileUpload } from 'primereact/fileupload';
 import { InputMask } from 'primereact/inputmask'
+import { useSearchParams } from 'react-router-dom';
+import { Button } from 'primereact/button';
+import { ContextMenu } from 'primereact/contextmenu';
 
 export default function SchedulePage() {
 
-  const [scheduleData, setScheduleData] = useState([])
-  const classTypes = ['LECTURE', 'LAB', 'PRACTICE', 'OTHER']
+  const [scheduleData, setScheduleData] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [selectedRow, setSelectedRow] = useState();
+  const [contextMenuData, setContextMenuData] = useState({})
+  const classTypes = ['LECTURE', 'LAB', 'PRACTICE', 'OTHER'];
+  const cm = useRef(null);
+  const cmItems = [
+    { label: 'Insert row above', icon: 'pi pi-angle-up', command: () => insertRowAbove()},
+    { label: 'Insert row below', icon: 'pi pi-angle-down', command: () => insertRowBelow()},
+    { label: 'Delete row', icon: 'pi pi-fw pi-trash', command: () => deleteRow()}
+];
 
   useEffect(() => {
     setScheduleData(defaultData);
   }, [])
 
-  function getDay(dayName){
-    switch(dayName){
-      case 'Monday':
-        return 'Понеділок'
-      case 'Tuesday':
-        return 'Вівторок'
-      case 'Wednesday':
-        return 'Середа'
-      case 'Thursday':
-        return 'Четвер'
-      case 'Friday':
-        return 'Пʼятниця'
-      case 'Saturday':
-        return 'Субота'
-      case 'Sunday':
-        return 'Неділя'
-      default:
-        return dayName
-    }
-  }
   function getClassType(value){
-    switch(value){
+    switch(value.toUpperCase()){
       case 'LECTURE':
         return 'info'
       case 'LAB':
@@ -61,8 +53,54 @@ export default function SchedulePage() {
     }
   }
 
+  function insertRowAbove(){
+    const updatedData = JSON.parse(JSON.stringify(scheduleData));
+    updatedData[contextMenuData.week]
+    .days[contextMenuData.day]
+    .classEntries.splice(contextMenuData.row, 0,
+    {
+      "className": "",
+      "startTime": "",
+      "endTime": "",
+      "classType": "",
+      "url": ""
+    });
+    setScheduleData(updatedData);
+  }
+  function insertRowBelow(){
+    const updatedData = JSON.parse(JSON.stringify(scheduleData));
+    updatedData[contextMenuData.week]
+    .days[contextMenuData.day]
+    .classEntries.splice(contextMenuData.row + 1, 0,
+    {
+      "className": "",
+      "startTime": "",
+      "endTime": "",
+      "classType": "",
+      "url": ""
+    });
+    setScheduleData(updatedData);
+  }
+  function deleteRow(){
+    const updatedData = JSON.parse(JSON.stringify(scheduleData));
+    updatedData[contextMenuData.week]
+    .days[contextMenuData.day]
+    .classEntries.splice(contextMenuData.row, 1);
+    setScheduleData(updatedData);
+  }
+
+  function handleContextMenu(e){
+    cm.current.show(e);
+    const selectedIndex = selectedRow;
+    const tableId = e.target.parentElement.parentElement.parentElement.parentElement.parentElement.id;
+    const [weekIndex, tableIndex] = tableId.split(' ');
+    setContextMenuData({week: weekIndex, day: tableIndex, row: selectedIndex});
+    console.log(contextMenuData);
+  }
+
   function completeEdit(weekIndex, dayIndex, e){
     if(e.value == e.newValue) return;
+    // handle update
     console.log(scheduleData[weekIndex].days[dayIndex].classEntries[e.rowIndex][e.field])
   }
 
@@ -87,30 +125,38 @@ export default function SchedulePage() {
   }
 
   function classTypeBodyTemplate(rowData){
+    if(rowData.classType == '') return <></>
     return <Tag value={rowData.classType} severity={getClassType(rowData.classType)}></Tag>
   }
 
   function uploadFile(){
-    console.log('file uploaded');
+    console.error('failed to upload file');
   }
 
   return (
     <>
+      <ContextMenu model={cmItems} ref={cm} breakpoint='767px'/>
       <div className='schedule-header'>
-        <div>Редактор розкладу</div><FileUpload mode='basic' accept='*' url="/api/upload" onUpload={uploadFile} chooseLabel='Завантажити файл'></FileUpload>
+        <div>Редактор розкладу</div>
+        <div className='header-button-container'>
+          <Button icon='pi pi-plus'>Створити новий розклад</Button>
+          <FileUpload mode='basic' accept='*' url="/api/upload" onUpload={uploadFile} chooseLabel='Завантажити файл'></FileUpload>
+        </div>
       </div>
       {scheduleData.map((week, weekIndex) => (
         <Card title={week.weekType == 'WEEK_A'? 'Тиждень A' : 'Тиждень B'} className='data-card' key={week.weekType}>
           <Accordion multiple>
             {week.days.map((day, dayIndex) => (
-              <AccordionTab header={getDay(day.dayName)} key={week.weekType + day.dayName}>
-                <DataTable value={day.classEntries} tableStyle={{ minWidth: '50rem' }} editMode="cell">
-                  <Column field='className' header='Урок' editor={(options) => textEditor(weekIndex, dayIndex, options)} onCellEditComplete={(e) => completeEdit(weekIndex, dayIndex, e)}></Column>
-                  <Column field='startTime' header='Початок' editor={(options) => timeEditor(weekIndex, dayIndex, options)} onCellEditComplete={(e) => completeEdit(weekIndex, dayIndex, e)}></Column>
-                  <Column field='endTime' header='Кінець' editor={(options) => timeEditor(weekIndex, dayIndex, options)} onCellEditComplete={(e) => completeEdit(weekIndex, dayIndex, e)}></Column>
-                  <Column field='classType' header='Тип уроку' body={classTypeBodyTemplate} editor={(options) => classTypeEditor(weekIndex, dayIndex, options)} onCellEditComplete={(e) => completeEdit(weekIndex, dayIndex, e)}></Column>
-                  <Column field='url' header='URL' editor={(options) => textEditor(weekIndex, dayIndex, options)} onCellEditComplete={(e) => completeEdit(weekIndex, dayIndex, e)}></Column>
-                </DataTable>
+              <AccordionTab header={day.dayName} key={week.weekType + day.dayName}>
+                <div onContextMenu={e => handleContextMenu(e)}>
+                  <DataTable id={`${weekIndex} ${dayIndex}`} selectionMode='single' selection={selectedRow} value={day.classEntries} tableStyle={{ minWidth: '50rem' }} editMode="cell" onRowMouseEnter={e => setSelectedRow(e.index)}>
+                    <Column field='className' header='Урок' editor={(options) => textEditor(weekIndex, dayIndex, options)} onCellEditComplete={(e) => completeEdit(weekIndex, dayIndex, e)}></Column>
+                    <Column field='startTime' header='Початок' editor={(options) => timeEditor(weekIndex, dayIndex, options)} onCellEditComplete={(e) => completeEdit(weekIndex, dayIndex, e)}></Column>
+                    <Column field='endTime' header='Кінець' editor={(options) => timeEditor(weekIndex, dayIndex, options)} onCellEditComplete={(e) => completeEdit(weekIndex, dayIndex, e)}></Column>
+                    <Column field='classType' header='Тип уроку' body={classTypeBodyTemplate} editor={(options) => classTypeEditor(weekIndex, dayIndex, options)} onCellEditComplete={(e) => completeEdit(weekIndex, dayIndex, e)}></Column>
+                    <Column field='url' header='URL' editor={(options) => textEditor(weekIndex, dayIndex, options)} onCellEditComplete={(e) => completeEdit(weekIndex, dayIndex, e)}></Column>
+                  </DataTable>
+                </div>
               </AccordionTab>
             ))}
           </Accordion>
